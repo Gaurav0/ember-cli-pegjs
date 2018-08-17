951532b1bff2d8dd2ef6b28fd99a2011c9ad376c
Gaurav Munjal <gaurav@peeriq.com>
2018-08-17 13:05:37 -0400

    Use my version of broccoli-pegjs

--- index.js
+++ index.js
@@ -4,23 +4,33 @@ var peg = require('broccoli-pegjs');
 
 module.exports = {
   name: 'ember-cli-pegjs',
+
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    app.registry.add('js', {
+    if (!app.options.pegOptions)
+      app.options.pegOptions = {};
+
+    if (!app.options.pegOptions.wrapper) {
+      app.options.pegOptions.wrapper = function (src, parser) {
+        return 'export default ' + parser;
+      }
+    }
+
+    this.pegOptions = app.options.pegOptions;
+
+    this.setupPreprocessorRegistry('parent', app.registry);
+  },
+
+  setupPreprocessorRegistry: function(type, registry) {
+    registry.add('js', {
       name: 'ember-cli-pegjs',
       ext: 'pegjs',
-      toTree: function(tree) {
-        if (!app.options.pegOptions)
-          app.options.pegOptions = {}
-
-        if (!app.options.pegOptions.wrapper) {
-          app.options.pegOptions.wrapper = function (src, parser) {
-            return 'export default ' + parser
-          }
+      toTree: tree => {
+        if (!this.pegOptions) {
+          return tree;
         }
-
-        return peg(tree, app.options.pegOptions);
+        return new peg(tree, this.pegOptions);
       }
     });
   }
